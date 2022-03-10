d6df9702abf3c5ad2b104d777ea6acd379d716c1
milikiyas <milikiyas2019@gmail.com>
2022-03-10 15:51:51 -0800

    new attempt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,12 @@
 	 * 
 	 * Retur: alwas 0 (success)
 	 *
+	 *
 	 */
-	int main(void)
+
+   	int main(void)
 {
 	char *s = "\"Programming is like building a multilingual puzzle";
 	puts(s);
-	return(0);
+	return (0);
 }
