4532947a73839ff3cc120acc61c4a2f9221caf96
milikiyas <milikiyas2019@gmail.com>
2022-03-11 15:29:33 -0800

    new one

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 ((success))
+*/
+
+int main(void)
+
+{
+	char hello ='a';
+
+	char hi ='z';
+
+	for (hello = a; hello <= hi; ++hello)
+		putchar(hello);
+	return (0);
+}
