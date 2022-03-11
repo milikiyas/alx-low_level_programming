894a411dec106be39d922271e4b423562a754d52
milikiyas <milikiyas2019@gmail.com>
2022-03-10 23:31:24 -0800

    pits command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-	/**
-	 * main -Entry point
-	 * 
-	 * Retur: alwas 0 (success)
-	 *
-	 */
-   	int main(void)
+/**
+* main - Entry point
+*
+* Return: always 0 (success)
+*/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
-	return (0);
-};
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
