0145c259ad6210513ae29278f924d1781cc515dc
milikiyas <milikiyas2019@gmail.com>
2022-03-11 13:51:22 -0800

    capitiization, git push

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,15 +22,15 @@ int n, Ls;
 	if (Ls > 5)
 
 		{
-			printf("%s %d %s %d %s\n", "last digit of", n, "is", Ls, "and is greater than 5");
+			printf("%s %d %s %d %s\n", "Last digit of", n, "is", Ls, "and is greater than 5");
 		}
 	if (Ls == 0)
 		{
-			printf("%s %d %s %d %s\n", "last digit of", n, "is", Ls, "and is 0");
+			printf("%s %d %s %d %s\n", "Last digit of", n, "is", Ls, "and is 0");
 		}
 	if (Ls < 6 && Ls != 0)
 		{
-			printf("%s %d %s %d %s\n", "last digit of", n, "is", Ls, "and is less 6 and not 0");
+			printf("%s %d %s %d %s\n", "Last digit of", n, "is", Ls, "and is less 6 and not 0");
 		}
 /* your code goes there */
 	return (0);
