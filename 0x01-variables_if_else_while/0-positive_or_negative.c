f3d8866b33ad14c1e024f31ad62021cafd918988
milikiyas <milikiyas2019@gmail.com>
2022-03-11 12:11:08 -0800

    hellow orld

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,17 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry poin
+ *
+ * Return always 0 (success)
+*/
 int main(void)
 {
-		int n;
-		n = 0;
-		srand(time(0));
-	        n =n + rand() -RAND_MAX / 2;
+	int n;
+	n = 0;
+	srand(time(0))
+	n = n + rand() - RAND_MAX / 2;
 /* your code goes there */
-		return (0);
+	return (0);
 }
