3e308987d0aa3763e2b963eb0c6173049aaed9c2
milikiyas <milikiyas2019@gmail.com>
2022-03-11 14:48:40 -0800

    new file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,25 +15,24 @@ int main(void)
 
 int n, Ls;
 
-	char a[] = "Last digit of";
-	char b[] = "is";
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	Ls = n % 10;
-	scanf ("%d", &n);
-	if  (Ls > 5)
-
-		{
-			printf("%s %d %s %d %s\n", a, n, b, Ls, "and is greater than 5");
-		}
-	if (Ls == 0)
-		{
-			printf("%s %d %s %d %s\n", a, n, b, Ls, "and is 0");
-		}
-	if (Ls < 6 && Ls != 0)
-		{
-			printf("%s %d %s %d %s\n", a, n, b, Ls, "and is less than 6 and not 0");
-		}
+char a[] = "Last digit of";
+char b[] = "is";
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+Ls = n % 10;
+scanf("%d", &n);
+if  (Ls > 5)	
+{
+	printf("%s %d %s %d %s\n", a, n, b, Ls, "and is greater than 5");
+}
+if (Ls == 0)
+{
+	printf("%s %d %s %d %s\n", a, n, b, Ls, "and is 0");
+}		
+if (Ls < 6 && Ls != 0)
+{
+	printf("%s %d %s %d %s\n", a, n, b, Ls, "and is less than 6 and not 0");
+}
 /* your code goes there */
 	return (0);
 }
