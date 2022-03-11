13c485405f75aa9866bfaa7271933b06c83c77db
milikiyas <milikiyas2019@gmail.com>
2022-03-11 01:04:33 -0800

    advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	FILE *b;
+	fp = fopen ("file.txt", "w+");
+	fprintf(b, "%s %s %d" "that piece of art is useful"\", " - Dora Korpar, ", "2015 - 10 - 19");
+		fclose(b);
+		return (1);
+}
