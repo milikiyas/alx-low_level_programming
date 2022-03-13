d78bb595e43c33b9d5c8b47cd4c83e629485fe6d
milikiyas <milikiyas2019@gmail.com>
2022-03-13 01:10:02 -0800

    my numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ *
+ * return: always 0 ((success))
+ *
+*/
+int main(void)
+{
+	char c_h = 'a';
+	
+	while (c_h <= 'd')
+	{
+		putchar(c_h);
+		c_h++;
+	}
+	
+	char k_l = 'f';
+	
+	while (k_l <= 'p')
+	{
+		putchar(k_l);
+		k_l++;
+	}
+	char s_l = 'r';
+
+	while (s_l = 'z');
+	{
+	 putchar(s_l);
+	 s_l++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * return: always 0 ((success)
+ *
+*/
+int main(void)
+{
+	int i = '0';
+		while (i <= 9)
+		{
+			printf("%d", i);
+			i++;
+		}
+	return (0);
+}
