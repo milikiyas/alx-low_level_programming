370d6c1746d319e2740fb54699cb9ac52e1fd1c1
milikiyas <milikiyas2019@gmail.com>
2022-03-16 10:04:53 -0700

    to

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
 * 
-*jack_bauer - this function prints every minutes of the day from 00:00 o 23:59
+*jack_bauer - prints every minute of the day from 00:00 to 23:59
 *
-* @void - no arguments
+*@void - no argument
 */
 void jack_bauer(void)
 {
