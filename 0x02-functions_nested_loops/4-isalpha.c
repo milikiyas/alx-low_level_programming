6a8c95ddfa8191b065811da523636bc2c022d29e
milikiyas <milikiyas2019@gmail.com>
2022-03-16 04:12:52 -0700

    both upper ad lower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main"
+/** _isalpha - checks for alphabetic charcter
+ * @c: takes in a character
+ * Returns: 1 if uppercase and lowercase letter or 0 else
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c  <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		rturn (1);
+	}
+	else 
+	{
+		return (0);
+	}
