517119ca5d2e052c508df70889b23f8caa127a09
milikiyas <milikiyas2019@gmail.com>
2022-03-16 10:47:50 -0700

    summation

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function that adds two integrs togther
+ * @a: takes in an integer
+ * @b: takes in an integer
+ * Return: a + b result
+ */
+int add(int a, int b)
+{
+	retur (a +b);
+}
