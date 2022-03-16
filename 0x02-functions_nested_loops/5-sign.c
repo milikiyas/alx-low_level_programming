4fec17038abc9af4fafb3cec6e6c57cceed1c1e7
milikiyas <milikiyas2019@gmail.com>
2022-03-16 04:40:42 -0700

    sighn identifir

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * -sign.c - prints + if n is greater than zero or else print 0 if n is zero otherwise it print - if n is less than zero
+ *
+ *  @n: takes any number
+ *
+ *  Return: 1 if n is greater than zero -1 if n less than zero else 0 n equals to zero
+ *
+ */
+int print_sign(int n)
+{
+	int n;
+	char c = '+';
+	char b = '-';
+	char d = '0';
+
+	if (n > 0)
+	{
+		return (1);
+		_putchar(c);
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar(d);
+	}
+	else (n < 0)
+	{
+		return (-1);
+		_putchar(b);
+	}
+}
+
