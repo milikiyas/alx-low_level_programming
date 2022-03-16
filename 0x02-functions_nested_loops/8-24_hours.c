496b741c42fa0e22ce44750b7db827232ac02aeb
milikiyas <milikiyas2019@gmail.com>
2022-03-16 09:51:11 -0700

    new commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * jack_bauer - this function prints every minute of the day 00:00 to 23:59
+ * jack_bauer - function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
 
  *@voi - no arguement
  */
