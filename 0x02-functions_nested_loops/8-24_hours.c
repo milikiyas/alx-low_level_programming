d78ba80c8570020adeac887e511b07fb9616401b
milikiyas <milikiyas2019@gmail.com>
2022-03-16 09:45:47 -0700

    my new concept

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/*
+ *jack_bauer - prints every minute of the day 00:00 to 23:59
+ *
+ *
+ *@voi - no arguement
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
