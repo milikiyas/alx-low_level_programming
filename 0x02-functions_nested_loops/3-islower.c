471ef00d32cd8a5959a79e8fa09dd8eb68caee8c
milikiyas <milikiyas2019@gmail.com>
2022-03-16 04:05:26 -0700

    new commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,8 @@
 /**
  * _islower -returns 1 if lowercase alphabet and 0 if not
  *
- * @c: taks in a character 
- * Returns:0 for upper case 1 for lowr case alphabts
+ * @c: taks in a character
+ * Return: 0 for upper case 1 for lowr case alphabts
  */
 int _islower(int c)
 {
