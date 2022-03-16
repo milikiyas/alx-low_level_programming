2f7145f8905a4a2b34b810ff8740de93224f6314
milikiyas <milikiyas2019@gmail.com>
2022-03-16 09:23:49 -0700

    my lastattempt

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,20 +4,17 @@
  *
  * @n: takes a value of intger
  *
- * Return: last_digit  if n >= 0 || 0 if n < 0
+ * Return: lg  always (success))
  */
 int print_last_digit(int n);
 
 
 {
-	int last_digit;
+	int lg;
 
-	last_digit = n % 10;
-	if (last_digit < 0)
-	{
-		_putchar('last_digit' * -1);
-		return (0);
-	}
-	return (last_digit);
+	lg = n % 10;
+	if (lg < 0)
+	_putchar('lg' * -1);
+		return (lg);
 	_putchar('\n');
 }
