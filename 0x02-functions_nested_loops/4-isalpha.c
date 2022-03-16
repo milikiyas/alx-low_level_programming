8a016430b597a9838f20ea075105c6a46779f16b
milikiyas <milikiyas2019@gmail.com>
2022-03-16 04:21:47 -0700

    spacing

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,14 @@
 #include "main.h"
-/** _isalpha - checks for alphabetic character
+
+/**
+ *  _isalpha - checks for alphabetic character
+ *
  * @c: takes in a character
+ *
+ *
  * Return: 1 if uppercase and lowercase letter or 0 else
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 'A' && c  <= 'Z') || (c >= 'a' && c <= 'z'))
