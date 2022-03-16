9670b55df10c3413aeccb4a8f9dd078fc4c59cfb
milikiyas <milikiyas2019@gmail.com>
2022-03-16 09:56:30 -0700

    spelling error

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /*
- *jack_bauer - function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
-
- *@voi - no arguement
+ *jack_bauer - this function prints minuts of the day from 00:00 to 23:59
+ *
+ * @void - no arguments
  */
 void jack_bauer(void)
 {
