8e43df95f1e87d26eb0713571b3fcb7f4a8f7076
milikiyas <milikiyas2019@gmail.com>
2022-03-16 10:25:41 -0700

    hello

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,10 +27,11 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
+				_putchar(k / 10 + '0')
+				_putchar(k % 10 + '0')
 			}
 		}
+		_putchar('\n');
 	}
 }
 		
