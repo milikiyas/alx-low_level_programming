74050ae1f6e7919f0f72edc19fed9323c2c1b98c
milikiyas <milikiyas2019@gmail.com>
2022-03-17 11:20:01 -0700

    new line

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
- * main - test function for positive or negative
+ * main - test function for positive, negative or zero
  * Return: 0 always 
  */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
+
 	positive_or_negative(i);
 	return (0);
 }
