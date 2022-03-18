71ce822303acc59ba0a25fe82d3a15f8c3838c13
milikiyas <milikiyas2019@gmail.com>
2022-03-18 04:58:26 -0700

    new please show me how

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,11 +3,13 @@
 * mul - multiplies two integer
 * @a: takes in an integer
 * @b: takes in an integer
-* Return: always 0 ((success))
+*
+* Return: void
 */
 int mul(int a, int b)
 {
 	int product_;
 
 	product_ = a * b;
+	_putchar('\n');
 }
