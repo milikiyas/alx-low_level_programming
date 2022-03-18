13884fe108f217e942e623aba0620f4ff79c092a
milikiyas <milikiyas2019@gmail.com>
2022-03-18 05:46:03 -0700

    new oppo

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,10 +2,12 @@
 
 /**
  * print_numbers - entry point
+ *
  * Description: prints the number with _putchar
  *
  * Return: void
  */
+
 void print_numbers(void)
 {
 	for (int i = '0'; i <= '9'; i++)
