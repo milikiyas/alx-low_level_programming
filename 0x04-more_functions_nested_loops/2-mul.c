db1803a7ea23ad0e4350b3c397d6a5a801ad4e66
milikiyas <milikiyas2019@gmail.com>
2022-03-18 05:19:34 -0700

    new

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,11 +4,12 @@
 * @a: takes in an integer value
 * @b: takes in an integer value
 *
-* Return: void
+* Return: 1 always success
 */
 int mul(int a, int b)
 {
 	int product_;
 
 		product_ = a * b;
+		return (1);
 }
