06995a2b9cfa3356279f3453ef74c3d066b93a42
milikiyas <milikiyas2019@gmail.com>
2022-03-18 05:16:22 -0700

    new value

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* mul - Function multiplies two integers a and b
+* mul - Function multiplies two integers
 * @a: takes in an integer value
 * @b: takes in an integer value
 *
@@ -11,6 +11,4 @@ int mul(int a, int b)
 	int product_;
 
 		product_ = a * b;
-
-		return(0);
 }
