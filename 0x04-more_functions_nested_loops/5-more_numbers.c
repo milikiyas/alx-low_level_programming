f93b3f45fb51e06a1f5e2efdfca74881ea60cd1e
milikiyas <milikiyas2019@gmail.com>
2022-03-18 10:36:11 -0700

    new commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * more_numbers - entry point
- *
- * Description: prints 10 times the numbers, from 0 to 14 using _putchar
- * Return: void
- */
+* more_numbers - entry point
+*
+* Description: prints 10 times the numbers, from 0 to 14 using _putchar
+* Return: void
+*/
 void more_numbers(void)
 {
 	for (k = 1; k <= 10; k++)
@@ -14,8 +14,8 @@ void more_numbers(void)
 		{
 			tens = m / 10;
 			units = m % 10;
-			if (m > 9 && m <= 14)
-				_putchar(tens + '0');
+				if (m > 9 && m <= 14)
+					_putchar(tens + '0');
 			_putchar(units + '0');
 		}
 		_putchar('\n');
