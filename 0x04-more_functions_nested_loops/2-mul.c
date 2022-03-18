55fb5501dbd6302abc2310a4f6c484697d9179ea
milikiyas <milikiyas2019@gmail.com>
2022-03-18 05:05:31 -0700

    new file

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -9,7 +9,13 @@
 int mul(int a, int b)
 {
 	int product_;
+	if ((a <= 0 || b <= 0) && !(a <= 0 && b <= 0))
+	{
+	product_ = -(a * b);
+	}
+	else if (a >= 0 && b >= 0)
+	{
+		product_ = a * b;
 
-	product_ = a * b;
-	_putchar('\n');
+		return(0);`
 }
