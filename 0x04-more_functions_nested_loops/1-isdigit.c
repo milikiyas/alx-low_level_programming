9612e3593910a0839eed438dc4cb5207fbead746
milikiyas <milikiyas2019@gmail.com>
2022-03-18 03:39:44 -0700

    new commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,12 @@
 /**
  * _isdigit - this function checks for a digit (0 through 9)
  *
- * @c: takes in integer value 
+ * @c: takes in integer value
  * Return: 1 if c is digit, otherwise 0
  */
 int _isdigit(int c)
 {
-	if ( c >= 0 && c <= 9)
+	if (c >= 0 && c <= 9)
 	{
 		return (1);
 	}
