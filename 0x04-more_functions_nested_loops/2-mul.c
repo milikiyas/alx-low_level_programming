7b614347c4d3136d551a5a50e96eb30646ba39f9
milikiyas <milikiyas2019@gmail.com>
2022-03-18 04:23:16 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* mul - multiplies two integer
+* @a: takes in an integer
+* @b: takes in an integer
+* Return: always 0 ((success))
+*/
+int mul(int a, int b)
+{
+	int product_;
+
+	product_ = a * b;
+	return (0);
+}
