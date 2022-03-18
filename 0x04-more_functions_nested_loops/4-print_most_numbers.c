09771904ebd24f4c1b814860e26280efe442c90f
milikiyas <milikiyas2019@gmail.com>
2022-03-18 06:22:34 -0700

    new commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,22 @@
 #include "main.h"
 
 /**
+ *print_most_numbers - entry point
  *
+ * Description: prints the numbers with _putchar
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int num;
+	
+	for (num = '0'; num <= '9'; num++)
+	{
+		if (num == '2' || num == '4')
+			;
+		else
+			_putchar(num);
+	}
+	_putchar('\n');
+}
