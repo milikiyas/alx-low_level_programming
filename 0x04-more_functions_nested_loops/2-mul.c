12742d1937ed3156c3ae1a6ef1697b8bc297299b
milikiyas <milikiyas2019@gmail.com>
2022-03-18 05:09:54 -0700

    new value

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,21 +1,16 @@
 #include "main.h"
 /**
-* mul - multiplies two integer
-* @a: takes in an integer
-* @b: takes in an integer
+* mul - Function multiplies two integer
+* @a: takes in an integer value
+* @b: takes in an integer value
 *
 * Return: void
 */
 int mul(int a, int b)
 {
 	int product_;
-	if ((a <= 0 || b <= 0) && !(a <= 0 && b <= 0))
-	{
-	product_ = -(a * b);
-	}
-	else if (a >= 0 && b >= 0)
-	{
+
 		product_ = a * b;
 
-		return(0);`
+		return(0);
 }
