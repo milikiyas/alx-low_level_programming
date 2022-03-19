ac50d0daa1b2507f2ea80d03aecd6b7856577388
milikiyas <milikiyas2019@gmail.com>
2022-03-19 12:32:01 -0700

    new line

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int odd_prime, max_prime;
+	long long num = 612852475143;
+
+	odd_prime = 3;
+	max_prime = 0;
+
+	while (num % 2 == 0)
+	{
+		max_prime = 2;
+		num /= 2;
+	}
+	while (num != 1)
+	{
+		while (num % odd_prime == 0)
+		{
+			max_prime = odd_prime;
+			num /= odd_prime;
+		}
+			odd_prime = odd_prime + 2;
+	}
+			printf("%d", max_prime);
+}
