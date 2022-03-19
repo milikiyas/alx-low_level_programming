714f5a2dc5ab488963aaec0c559f5a29c63c259f
milikiyas <milikiyas2019@gmail.com>
2022-03-19 09:55:20 -0700

    new era

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,13 +10,16 @@
  void print_triangle(int size)
 {
 	int i, j, k;
+	  
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
 
 		for (i = 1; i <= size; i++)
 		{
-			if (size <= 0)
-			_putchar('\n');
 
-			else if ( size > 0)
+			if ( size > 0)
 			{
 				for (j = 1; j <= (size - i); j++)
 				{
