ca199099bb9740e47cb66e8225e95bb70acbac90
milikiyas <milikiyas2019@gmail.com>
2022-03-19 04:14:45 -0700

    new file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,21 +15,20 @@ void fizz_buzz(void)
 		{
 			if (i % 3 == 0 && !(i % 5 == 0))
 			{
-				printf("Fizz");
+				printf("Fizz\n");
 			}
 			else if ((i % 5 == 0)  && !(i % 3 == 0))
 					{
-					printf("Buzz");
+					printf("Buzz\n");
 					}
 			else if (i % 5 == 0 && i % 3 == 0)
 			{
-				printf("FizzBuzz");
+				printf("FizzBuzz\n");
 			}
 			else
 			{
-				printf("%d", i);
+				printf("%d\n", i);
 			}
-			putchar(' ');
 		}
 		printf("Buzz\n");
 }
