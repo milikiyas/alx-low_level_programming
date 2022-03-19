24e4d5aab782aa125ae90ed9a2e97fd203cb0c4f
milikiyas <milikiyas2019@gmail.com>
2022-03-19 04:46:32 -0700

    new line

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-for (i = 1; i < 100; i++)
+for (i = 1; i <= 100; i++)
 	{
 	if (i % 3 == 0 && i % 5 == 0)
 
@@ -26,6 +26,6 @@ for (i = 1; i < 100; i++)
 	else
 		printf("%d\n", i);
 	}
-	printf("Buzz\n");
+
 	return (0);
 }
