b1317c1c40f1b73f41799f5accbf4380f89427f4
milikiyas <milikiyas2019@gmail.com>
2022-03-18 21:25:10 -0700

    7th attempt

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,16 +16,15 @@ void print_diagonal(int n)
 		{
 			_putchar('\n');
 		}
-		else if (n > 0)
-		{
+
 			for (j = 1; j <= n; j++)
 			{
-				for (i = 0; i <= i -1; i++)
+				for (i = 0; i <= j - 1; i++)
 				{
 					_putchar(' ');
 				}
 				_putchar(k);
 				_putchar('\n');
 			}
-		}
+
 }
