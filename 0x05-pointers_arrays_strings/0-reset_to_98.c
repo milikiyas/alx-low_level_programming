ddaef208eda73d4085806a86e5f59cfff67f5c47
milikiyas <milikiyas2019@gmail.com>
2022-03-21 10:09:14 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - entry point
+ * @n: takes integer type of adrresses of var
+ * Description: takes adress of int variable and change to 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
