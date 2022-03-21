d2b3416abc62bf1090053eb19249b65c2bd81be2
milikiyas <milikiyas2019@gmail.com>
2022-03-21 10:22:00 -0700

    new file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,14 @@
 #include "main.h"
+
 /**
  * reset_to_98 - entry point
  * @n: takes integer type of adrresses of var
  * Description: takes adress of int variable and change to 98
  * Return: void
  */
+
 void reset_to_98(int *n)
 {
-	*n = 98;
+
+	*(&n) = 98;
 }
