d9d9c8892be638fe1b03de2c114dcd3b537e40e1
milikiyas <milikiyas2019@gmail.com>
2022-03-22 00:36:22 -0700

    wow ala

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - entry point
+ * @s: takes in a string value
+ * Description: prints string in reverse
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (s[i - 1] != '\0')
+	{
+		_putchar(s[i - 1]);
+		i--;
+	}
+}
+
+
