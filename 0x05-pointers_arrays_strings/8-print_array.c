10e2c388b164a3b603023a4ef8a001770042fc5d
milikiyas <milikiyas2019@gmail.com>
2022-03-22 13:35:02 -0700

    new eski

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_array - entyr point
+ * @a: takes adress of a variable
+ * @n: takes an integer value
+ * Description: prints out values of an array
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+	
+	for (i = 0; *(a + i); i++)
+	{
+	       if (i = n -1)
+	       {
+		break;
+	       }	
+	_putchar(a[i]);
+	_putchar(',');
+	_putchar(' ');
+	}
+	_putchar('\n');
+}t
+
+
