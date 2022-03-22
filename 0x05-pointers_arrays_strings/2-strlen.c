d5783341a1f62cad07c6ae064dbbb566d38e434b
milikiyas <milikiyas2019@gmail.com>
2022-03-21 22:07:19 -0700

    new eski

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen - entry point
+ * @s: takes character type value
+ * Return:  always 0
+ *
+ */
+int _putchar(char c);
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	_putchar(i + '0');
+	return(0);
+}
