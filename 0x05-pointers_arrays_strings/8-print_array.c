ad6ff5e9ed6b66b007c013d26dd7580fc4016745
milikiyas <milikiyas2019@gmail.com>
2022-03-22 16:05:36 -0700

    new era

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,22 +11,20 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (i = 0; i <= (n-1); i++)
+	for (i = 0; i < (n-1); i++)
 	{
-	if (i <= (n - 1))
-	       {
-		if (i < n - 1)
+		if (i < (n - 1))
 		{
-			printf("%d, ", *(a+i));
+		printf("%d, ", a[i]);
 		}
-	       
-		else
-		{
-		printf("%d", *(a + i));
-		}
-	       }
-	       
+	
+		else if (i == (n - 1))
+		 {
+			 printf("%d", a[i]);
+		 }
+				 	 
 	}
+	
 	_putchar('\n');
 }
 
