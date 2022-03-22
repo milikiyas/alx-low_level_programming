2a2d79d2b8ff46ec883abf43d50c3c4e6aa68777
milikiyas <milikiyas2019@gmail.com>
2022-03-22 11:41:29 -0700

    one pass

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts2 - entry point
+ * @s: takes a adress of a string type value
+ * Description: prints every other character of a string, starting with the first character
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+	for( i = 0; *(str + i) != '\0'; i += 2)
+		_putchar(i + '0');
+}
+
