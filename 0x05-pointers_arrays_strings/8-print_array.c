b234701c80a5223ffd0033f767559346ca0eea30
milikiyas <milikiyas2019@gmail.com>
2022-03-22 13:44:08 -0700

    new era

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,10 +15,13 @@ void print_array(int *a, int n)
 	       if (i == (n - 1))
 	       {
 		break;
-	       }	
-	_putchar(a[i] + '0');
-	_putchar(',');
-	_putchar(' ');
+	       }
+	       else
+	       {	       
+		_putchar(a[i] + '0');
+		_putchar(',');
+		_putchar(' ');
+	       }
 	}
 	_putchar('\n');
 }
