770365750ddcb495cb39b34780d9d57814166901
milikiyas <milikiyas2019@gmail.com>
2022-03-22 11:53:09 -0700

    new line

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
  * puts2 - entry point
- * @s: takes a adress of a string type value
- * Description: prints every other character of a string, starting with the first character
+ * @str: takes a adress of a string type value
+ * Description: prints every other character of a string
  * Return: void
  */
 void puts2(char *str)
 {
 	int i;
-	for( i = 0; *(str + i) != '\0'; i += 2)
+
+	for (i = 0; *(str + i) != '\0'; i += 2)
 		_putchar(str[i]);
+	_putchar('\n');
 }
 
