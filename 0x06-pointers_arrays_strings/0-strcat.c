2bfa6f6eb2498d98983ed78d6316a337159a82b1
milikiyas <milikiyas2019@gmail.com>
2022-03-25 11:07:56 -0700

    array first time

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include <main.h>
+/**
+ * _strcat - entry point
+ * @dest: taks char type variable
+ * @src: takes char type variable
+ * Description:  concatenates two strings.
+ * Return: dest charactr type pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int k;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		;
+	}
+	for (k = 0; dest[i] != '\0'; k++)
+	{
+		_putchar(dest[i] + '0');
+	}
+
+	for (f = 0; f <= i - 1; f++)
+	{
+		dest[k] = src[f];
+		k++;
+		_putchar(dest[k] + '0');
+	}
+	return (dest);
+}
