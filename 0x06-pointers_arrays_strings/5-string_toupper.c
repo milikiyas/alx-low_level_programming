b37d84c370369a0d014bebffe8f51afa6550ae67
milikiyas <milikiyas2019@gmail.com>
2022-03-27 14:08:46 -0700

    new era

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,16 +7,22 @@
  */
 char *string_toupper(char *str)
 {
-	
+
 	int i, j;
-	
+
 	for (j = 0; str[j] != '\0'; j++)
 		;
 	j = j - 1;
 
 	for (i = 0; i < j; i++)
 	{
+
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
 		str[i] = (str[i]) - 32;
+	}
+	else
+		str[i] = str[i];
 	}
 return (str);
 }
