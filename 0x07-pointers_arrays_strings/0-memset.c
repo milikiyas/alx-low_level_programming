d4e4a1db889c9d681c8de465935a24fd1a24b222
milikiyas <milikiyas2019@gmail.com>
2022-03-28 00:42:30 -0700

    new line

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,13 +3,13 @@
  * _memset - entry point
  * @s - takes in character type pointer
  * @b: takes in character type value
- * @n: takes in unsighned integer value 
+ * @n: takes in unsighned integer value
  * Return: s pointer
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 		s[i] = b;
 	return (s);
