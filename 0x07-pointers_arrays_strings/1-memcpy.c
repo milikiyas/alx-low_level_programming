1f5d9572160eff1a11cdfc9ffaf3a82d2fb7bbda
milikiyas <milikiyas2019@gmail.com>
2022-03-28 06:00:17 -0700

    new

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	int i, p;
 
 	if (dest == NULL)
-		return NULL;
+		return (NULL);
 	if (dest)
 	{
 	for (i = 0; i < n; i++)
@@ -25,5 +25,4 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 		dest[i + p] = src[i];
 	}
 	return (dest);
-}
-	
+}	
