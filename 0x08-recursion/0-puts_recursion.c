ba11d79cfae656bebea5f899bbc534dbb1a34e28
milikiyas <milikiyas2019@gmail.com>
2022-03-30 05:59:53 -0700

    new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - main entry
+ * @s: takes in pointer to string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0', i++)
+		_putchar(*s);
+		_putchar('\n');
+}
