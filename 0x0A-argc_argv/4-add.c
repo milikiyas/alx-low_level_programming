12ba05d1e8c3eeb59a22a4c9ed9d5e8191bfb0c9
milikiyas <milikiyas2019@gmail.com>
2022-04-01 13:47:18 -0700

    how about this

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - entry point
+ * @argv: a array of string
+ * @argc: size of arguments 
+ * Return: 0  on success oherwise 1 for negaive arguments
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, i;
+
+	for (i = 1; i <= argc; i++)
+	{
+		if (argv[i] > '0' && !(argv[i]
+		{
+			sum = sum + argv[i];
+		}
+		elseif (argv[i] <= 
