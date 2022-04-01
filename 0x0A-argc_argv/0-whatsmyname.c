15216c6d2cb21919a2e3e99e6986a947da2db1d9
milikiyas <milikiyas2019@gmail.com>
2022-04-01 10:31:03 -0700

    wow all this days

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - entry point
+ * @argv: array of strings
+ * @argc: size of the array plus one
+ * Return: 0 always
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
