d08d1f6cda631711eee56f748e2ce14da0e67f9f
milikiyas <milikiyas2019@gmail.com>
2022-04-01 10:17:45 -0700

    new line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - entry point
+ * @argv: array of string
+ * @argc: number of arguements passed
+ * Return: 0 always on success
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printtf("%d\n", argc -  1);
+	return (0);
+}
