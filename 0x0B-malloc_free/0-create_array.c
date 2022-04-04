8e4ce7c2c534373881e9b014cfccb44013497080
milikiyas <milikiyas2019@gmail.com>
2022-04-04 11:35:09 -0700

    characye only

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_array - entry point
+ * @size: takes positive integer for size of array
+ * @c: character for the array
+ * Return: pointer if size is not 0  otherwise NULL
+ */
+
+
+	char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	int i;
+
+	ptr = malloc(size * sizeof(char));
+	if (size == 0)
+		return (NULL);
+
+	for (i = 0; i <= size; i++)
+		*(ptr + i) = c;
+	return (ptr);
+
+}
