3f4440037219c58bc0cf8561656ee70d6794e190
milikiyas <milikiyas2019@gmail.com>
2022-04-04 11:56:51 -0700

    new commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,11 +15,15 @@ char *create_array(unsigned int size, char c)
 	ptr = malloc(size * sizeof(char));
 	if (size == 0)
 		return (NULL);
-
-	for (i = 0; i <= size; i++)
+	else
+	{	
+		for (i = 0; i <= size; i++)
+		{	
 		*(ptr + i) = c;
-	free (ptr);
+		}
+		free(ptr);
+	
 	return (ptr);
-
+	}
 
 }
