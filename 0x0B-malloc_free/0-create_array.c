2667cb360501ad6b1878c21c5d92543950b6d197
milikiyas <milikiyas2019@gmail.com>
2022-04-04 12:10:54 -0700

    new

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	ptr = (void *)malloc(size * sizeof(char));
+	ptr = (char *)malloc(size * sizeof(char));
 	if (size == 0)
 	{
 
