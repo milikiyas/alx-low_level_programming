1a5762d033c1252111d22c6ab9f0dcccf7861af9
milikiyas <milikiyas2019@gmail.com>
2022-04-05 00:50:48 -0700

    new

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strdup - entry point
+ * @str: array of string
+ * Return: pointer to character on SUCCESS otherwise NULL
+ */
+char *_strdup(char *str)
+{
+	int i, k;
+	char *ptr;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	ptr = (char *) malloc((i + 1) * sizeof(char));
+	if (i == 0)
+		return (NULL);
+
+	if (ptr == NULL)
+		return (NULL);
+		k = 0;
+		while (k <= i)
+		{
+			ptr[k] = str[k];
+			k++;
+		}
+
+		ptr[k] = '\0';
+			return (ptr);
+}
