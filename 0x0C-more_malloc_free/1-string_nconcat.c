dd2e0d6105b232728aa684a44236a7268be71868
milikiyas <milikiyas2019@gmail.com>
2022-04-07 06:24:37 -0700

    new line

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,6 +23,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 
 	ptr = malloc(i + n + 1 * sizeof(char));
+	
+	if (ptr == NULL)
+		return (NULL);
 	for (k = 0; k < i; k++)
 		ptr[k] = s1[k];
 
