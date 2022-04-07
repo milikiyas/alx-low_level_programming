858bd3a4ac8e4c968b537f138161330155424ff1
milikiyas <milikiyas2019@gmail.com>
2022-04-07 06:51:31 -0700

    new

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,11 +15,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr;
 	unsigned int k, i, j;
 
-	if (s1 == NULL || s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
 		s2 = "";
-	}
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
