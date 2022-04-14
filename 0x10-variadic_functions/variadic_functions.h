fb8dddfff1af583a5267085d451bb8e70fa18f86
milikiyaswosenseged <99360267+milikiyas@users.noreply.github.com>
2022-04-14 17:57:40 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,12 +1,7 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-/*
- * File: variadic_functions.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0F-variadic_functions directory.
- */
+
 
 #include <stdarg.h>
 
